8d7adb653d04aa62cc3a9bf89c1b803238d5742a
abcdemi <85865058+abcdemi@users.noreply.github.com>
2022-02-23 23:15:49 +0300

    Update Maximal-square.py

--- Maximal-square.py
+++ Maximal-square.py
@@ -6,11 +6,11 @@ def maximalSquare(matrix):
         for j in range(n):
             if matrix[i][j] == '1':
                 dp[i][j] = min(dp[i-1][j] if i > 0 else 0, \
-                    dp[j-1][i] if j > 0 else 0,\
+                    dp[i][j-1] if j > 0 else 0,\
                         dp[i-1][j-1] if i > 0 and j > 0 else 0) + 1
                 if dp[i][j] > result:
                     result = dp[i][j]
     return result * result
 
-A = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+A = [["1","1","1","1","0"],["1","1","1","1","0"],["1","1","1","1","1"],["1","1","1","1","1"],["0","0","1","1","1"]]
 print(maximalSquare(A))
